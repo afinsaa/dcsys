870fe96feb2a838930172cd4d71ad11fa6aa7366
answaza <afinsaa1@gmail.com>
2021-10-03 21:48:53 +0300

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qr_code_reader_demo_production"
 
   config.action_mailer.perform_caching = false
-
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'c921dd84886cb2',
+    :password => '8e8dab4f489a8d',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
