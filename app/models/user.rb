80617f14ef015796a77c18b8117c29561d40ae39
answaza <afinsaa1@gmail.com>
2021-10-03 22:15:30 +0300

    school shall not be a must

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   
-  belongs_to :school
+  belongs_to :school, optional: true
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
